e6dcb5a5247e2c9357b855bdedee6d688ef035e1
Salman Rahman <salman.rahman515@gmail.com>
2024-05-08 19:32:44 -0400

    added java 8 and jruby setting script for cmd session

---
+++ set_java_8_jruby_version.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+@REM Example Java Path: C:\Program Files\Java\jdk-1.8\bin\java.exe
+@REM Example JRuby Path: C:\<path_to_jruby>\jruby-dist-9.1.5.0-bin\jruby-9.1.5.0\bin\jruby.exe
+
+set JAVA_HOME=C:\Program Files\Java\jdk-1.8
+set JRUBY_HOME=C:\Projects\Life_Manager_Backend_Root\thirdparty\tabula\jruby\jruby-dist-9.1.5.0-bin\jruby-9.1.5.0
+
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Java version set for this session:
+java -version
+if %ERRORLEVEL% neq 0 (
+    echo Failed to set Java version correctly.
+    goto end
+)
+
+echo Setting up JRuby in the session PATH:
+set PATH=%JRUBY_HOME%\bin;%PATH%
+echo JRuby version set for this session:
+jruby.exe -v
+if %ERRORLEVEL% neq 0 (
+    echo Failed to access JRuby version.
+    goto end
+)
+
+:end
+cmd
+
+endlocal
